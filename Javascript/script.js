e02c4ec7e804aa9dde9bc4b7df1fdafabd99c542
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-07-06 12:29:18 +0500

    Advanced javascript

---
+++ Javascript/script.js
@@ -0,0 +1,17 @@
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+var name = "Hamid Zahid"
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+
+
+const fsfname = "Thara Nhai Op"
+console.log(fsfname);
+
+let fname = "Guru Bhai Sabbbb"
+console.log(fname);
