2ba8682cac2ddcd5d6cfb9935157874df8eaa278
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-07-06 12:47:05 +0500

    Advanced javascript

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,17 +1,25 @@
-console.log(name);
-console.log(name);
-console.log(name);
-console.log(name);
-console.log(name);
-var name = "Hamid Zahid"
-console.log(name);
-console.log(name);
-console.log(name);
-console.log(name);
+// let And Const Variables
+// console.log(name);
+// var name = "Hamid Zahid"
+// console.log(name);
 
 
-const fsfname = "Thara Nhai Op"
-console.log(fsfname);
+// const fsfname = "Thara Nhai Op"
+// console.log(fsfname);
 
-let fname = "Guru Bhai Sabbbb"
-console.log(fname);
+// let fname = "Guru Bhai Sabbbb"
+// console.log(fname);
+// fname ="heloo bhai"
+// console.log(fname);
+
+// simple Function ES5
+function Greet(name, age,value ,time ,money){
+    return "Hey Baby " + name  + " " + age + " " + value + " " + time + " " +money;
+}
+console.log(Greet("Hamid Eventuilly", "Second Value" ,'78',"helloo","World"))
+// Arrow Function ES6
+var greet = (name , age,value ,time ,money) =>{
+    return "Hello " + name + " " + age + " " + value + " " + time + " " +money;
+}
+
+console.log(greet("Hamid Eventuilly", "Second Value" ,'78',"helloo","World"))
